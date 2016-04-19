b6ee2b7a7c42e1724f5cfd4a0257ecf220c36262
fjcamillo <fjcamillo.dev@gmail.com>
2016-04-19 15:24:55 +0800

    Working Question2 and 3 plus a Django Site

---
+++ Site/TitanicSite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TitanicsiteConfig(AppConfig):
+    name = 'TitanicSite'
--- titanicAnalysis.py
+++ titanicAnalysis.py
@@ -54,18 +54,47 @@ def get_question_two(data):
     # '#'Get the total number of survivors from the data set
     total_number_of_survivors = data['Survived'].sum()
 
-    return number_of_male_ob, number_of_female_ob, total_number_of_survivors, male_count, female_count
+    total_count = data['Sex'].count()
+
+    return number_of_male_ob, number_of_female_ob, total_number_of_survivors, male_count, female_count, total_count
 
 
 def get_question_three(data):
     per_w_sibsp = 0
+    survivors_w_sibsp = 0
 
-    for i in range(1, len(data['Sibsp']), 1):
-        test_sibsp = list(data.iloc[i:i, "Sibsp"])
-        
+    for i in range(1, len(data['Sibsp'])+1, 1):
+        test_sibsp = list(data.loc[i:i, "Sibsp"])
+        survived = list(data.loc[i:i, "Survived"])
+        if int(str(test_sibsp[0])) > 0:
+            per_w_sibsp += 1
+            if survived[0] == 1:
+                survivors_w_sibsp += 1
+        elif int(str(test_sibsp[0])) == 0:
+            per_w_sibsp += 1
+        else:
+            pass
+
+    total_sibsps = data['Sibsp'].sum()
+
+    return per_w_sibsp, survivors_w_sibsp, total_sibsps
+
 
 def get_question_four(data):
-    pass
+    thirdclass_total = 0
+    secondclass_total = 0
+    firstclass_total = 0
+    for i in range(1, len(data['Pclass']) + 1, 1):
+        actual_class = list(data.loc[i:i, "Pclass"])
+        survived = list(data.loc[i:i, "Survived"])
+        if int(actual_class[0]) == 1:
+            firstclass_total += 1
+        elif int(actual_class[0]) == 2:
+            secondclass_total += 1
+        elif int(actual_class[0]) == 3:
+            thirdclass_total += 1
+        else:
+            pass
 
 
 def get_question_five(data):
@@ -80,15 +109,26 @@ def get_question_seven(data):
     pass
 
 
+def get_question_eight(data):
+    pass
+
+
+def get_question_nine(data):
+    pass
+
 
 def main():
     question_two = get_question_two(titanic)
-    # print(question_two)
-    plt.plot(2, question_two[2], label="Question2")
-    plt.title("Question 2")
-    plt.xlabel("Gender")
-    plt.ylabel("Number of Survivors")
-    plt.show()
+    print("Question #2 Answer \n"
+          "There are a total of %s persons on the data set \n"
+          "There are %s Men and %s Women on board\n"
+          "of the %s on-board only %s survived\n"
+          "%s of those are men, while the remaining %s are women\n"
+          % (question_two[5], question_two[0], question_two[1], question_two[5],
+             question_two[2], question_two[3], question_two[4]))
+
+    question_three = get_question_three(titanic)
+    print(question_three, "Answers on question 3")
 
 if __name__ == '__main__':
     main()
